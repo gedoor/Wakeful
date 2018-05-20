7b69d1341190a8b199021767dcf0e32c84dc6472
gedoor <gekunfei@live.com>
2018-05-20 17:59:06 +0800

    优化

--- app/src/main/java/com/gedoor/wakeful/MainActivity.java
+++ app/src/main/java/com/gedoor/wakeful/MainActivity.java
@@ -1,16 +1,41 @@
 package com.gedoor.wakeful;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.vw_zfb_tz)
+    CardView vwZfbTz;
+    @BindView(R.id.vw_zfb_hb)
+    CardView vwZfbHb;
+    @BindView(R.id.vw_zfb_rwm)
+    CardView vwZfbRwm;
+    @BindView(R.id.vw_wx_rwm)
+    CardView vwWxRwm;
+    @BindView(R.id.vw_qq_rwm)
+    CardView vwQqRwm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        vwZfbTz.setOnClickListener(view -> Donate.aliDonate(this));
+        vwZfbHb.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW,
+                "https://render.alipay.com/p/f/fd-j6lzqrgm/guiderofmklvtvw.html?shareId=2088002567472225&campStr=p1j%2BdzkZl018zOczaHT4Z5CLdPVCgrEXq89JsWOx1gdt05SIDMPg3PTxZbdPw9dL&sign=OqN%2FEToAWmYOBperSICGSOtBs36JILq1%2BshguP6l51U%3D&scene=offlinePaymentNewSns"));
+        vwZfbRwm.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, "https://gedoor.github.io/MyBookshelf/zfbskrwm.jpg"));
+        vwWxRwm.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, "https://gedoor.github.io/MyBookshelf/wxskrwm.jpg"));
+        vwQqRwm.setOnClickListener(view -> openIntent(Intent.ACTION_VIEW, "https://gedoor.github.io/MyBookshelf/qqskrwm.jpg"));
     }
 
     // 添加菜单
@@ -31,5 +56,16 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    void openIntent(String intentName, String address) {
+        try {
+            Intent intent = new Intent(intentName);
+            intent.setData(Uri.parse(address));
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, R.string.can_not_open, Toast.LENGTH_SHORT).show();
+        }
     }
 }
